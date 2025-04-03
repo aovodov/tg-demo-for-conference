69fb3fec85d03702a882ddefd8de6a8819fc73f0
Artem Ovodov <aovodov@qatools.ru>
2025-04-03 15:52:04 +0300

    added request parameters

--- src/main/java/org/example/WebhookController.java
+++ src/main/java/org/example/WebhookController.java
@@ -1,8 +1,10 @@
 package org.example;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
@@ -12,9 +14,20 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 @RequestMapping("/webhook")
 @RequiredArgsConstructor
 public class WebhookController {
+
+    @Value("${telegram.bot.secret}")
+    private String botSecret;
+
     private final TelegramDemoBot bot;
     @PostMapping("/bot")
-    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
+    public BotApiMethod<?> onUpdateReceived(
+            @RequestBody Update update,
+            @RequestHeader(value = "X-Telegram-Bot-Api-Secret-Token", required = true) String secretToken) {
+
+        if (!botSecret.equals(secretToken)) {
+            throw new RuntimeException("Invalid secret token");
+        }
+
         return bot.onWebhookUpdateReceived(update);
     }
 
